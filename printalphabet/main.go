1b522cdf9d330c2b79598fa3ac694a5f49eb9847
abdykarimovakhan <54791050+abdykarimovakhan@users.noreply.github.com>
2019-10-16 11:31:48 +0600

    Update main.go

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-	var aRune string = "abcdefghijklmnopqrstuvwxyz/n"
+	var aRune string = "abcdefghijklmnopqrstuvwxyz\n"
 
 	z01.PrintRune(rune(aRune[0]))
 	z01.PrintRune(rune(aRune[1]))
