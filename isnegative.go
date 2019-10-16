b9a37a09d32c9584c35e79ab3de0ae5dd7dcc9c5
akhanabdykarimov <akhanabdykarimov@gmail.com>
2019-10-16 15:11:26 +0600

    asa

--- isnegative.go
+++ isnegative.go
@@ -1,12 +1,12 @@
-package main
+package piscine
 
 import "github.com/01-edu/z01"
 
 func IsNegative(nb int) {
-	int := 1
 	if nb < 0 {
 		z01.PrintRune('T')
-	} else if int <= 0 {
+	} else int <= 0 {
 		z01.PrintRune('F')
 	}
+	z01.PrintRune(10)
 }
