18b61b82c136b8a1ce1cc532cb9cff2822690784
akhanabdykarimov <akhanabdykarimov@gmail.com>
2019-10-16 14:32:03 +0600

    a

--- isnegative.go
+++ isnegative.go
@@ -2,13 +2,11 @@ package main
 
 import "github.com/01-edu/z01"
 
-func main() {
-
+func IsNegative(nb int) {
 	int := 1
-
-	if int >= 0 {
-		z01.Println("true")
+	if nb < 0 {
+		z01.PrintRune('T')
 	} else if int <= 0 {
-		z01.Println("false")
+		z01.PrintRune('F')
 	}
 }
