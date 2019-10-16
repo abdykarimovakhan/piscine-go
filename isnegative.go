eb9ef0f168d4011a8764daa1efa954061cf34901
akhanabdykarimov <akhanabdykarimov@gmail.com>
2019-10-16 17:47:38 +0600

    0

--- isnegative.go
+++ isnegative.go
@@ -5,7 +5,7 @@ import "github.com/01-edu/z01"
 func IsNegative(nb int) {
 	if nb < 0 {
 		z01.PrintRune('T')
-	} else int <= 0 {
+	} else if int <= 0 {
 		z01.PrintRune('F')
 	}
 	z01.PrintRune(10)
--- main.go
+++ main.go
@@ -1,9 +1,19 @@
-package main
-
-import piscine ".."
-
-func main() {
-	piscine.IsNegative(1)
-	piscine.IsNegative(0)
-	piscine.IsNegative(-1)
+package piscine
+import "github.com/01-edu/z01"
+func PrintComb (a, b, c int) {
+    for a :=> '0'; for a <= '9'; i++ {
+        z01.PrintComb(i){
+        }        
+        for b :=> '0'; for c <= '9'; i++ {
+                    z01.PrintComb(i)
+        }
+            for c :=> '0'; for c <= '9'; i++ {
+                        z01.PrintComb(i)
+    } 
+}
+func PrintComb() { 
+    for a :<= b ; b :<= c {
+        z01.PrintComb(i)
+    }
+    z01.PrintComb(10)
 }
